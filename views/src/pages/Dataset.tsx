f05eb77b07860d6e9b7d98c6db28c4ba95bac7e0
Arnab Hazra <arnhazra@gmail.com>
2023-02-18 00:55:53 +0530

    autodeploy

--- views/src/pages/Dataset.tsx
+++ views/src/pages/Dataset.tsx
@@ -18,6 +18,7 @@ import contractAddress from '../constants/Address'
 import endPoints from '../constants/Endpoints'
 import { GlobalContext } from '../context/globalStateProvider'
 import DatasetStoreHeader from '../modules/DatasetStoreHeader'
+import { toast } from 'react-hot-toast'
 declare const window: any
 const web3 = new Web3(Web3.givenProvider)
 
@@ -98,15 +99,7 @@ const ViewOneDataSetPage: FC = () => {
     const dataset = useViewDataSet({ id: datasetId })
     const subscriptionStatus = useIsSubscribed({ id: datasetId, hasClickedSubscribed })
     const similarDatasets = useFindSimilarDatasets({ id: datasetId })
-    const [fromAccount, setFromAccount] = useState('')
-
-    useEffect(() => {
-        async function loadAccounts() {
-            const accounts = await web3.eth.getAccounts()
-            setFromAccount(accounts[0])
-        }
-        loadAccounts()
-    }, [])
+    const [account, setAccount] = useState('')
 
     const subscribe = async () => {
         if (dataset.price === 0) {
@@ -115,9 +108,24 @@ const ViewOneDataSetPage: FC = () => {
         }
 
         else {
-            const contract = new web3.eth.Contract(tokenABI as any, contractAddress.tokenContractAddress)
-            await contract.methods.transfer(contractAddress.tokenContractAddress, web3.utils.toWei(dataset.price.toString(), 'ether')).send({ from: fromAccount })
-            await axios.post(`${endPoints.subscribeEndpoint}/${datasetId}`)
+            try {
+                if (typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+                    try {
+                        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+                        setAccount(accounts[0])
+                        const contract = new web3.eth.Contract(tokenABI as any, contractAddress.tokenContractAddress)
+                        await contract.methods.transfer(contractAddress.tokenContractAddress, web3.utils.toWei(dataset.price.toString(), 'ether')).send({ from: account })
+                        await axios.post(`${endPoints.subscribeEndpoint}/${datasetId}`)
+                        setClickedSubscribed(true)
+                    } catch (err) {
+                        toast.error('Unable to connect to metamask')
+                    }
+                } else {
+                    toast.error('Please install metamask')
+                }
+            } catch (error) {
+                toast.error('Please install metamask')
+            }
         }
     }
 
@@ -147,10 +155,10 @@ const ViewOneDataSetPage: FC = () => {
                                 <button className='chip'>{dataset.price === 0 ? 'FREE' : `${dataset.price} LST`}</button>
                                 <button className='chip'>{dataset.dataLength} Datapoints</button><br />
                             </div>
-                            <button disabled={subscriptionStatus.isSubscribed} className='btn' onClick={subscribe}>
+                            {account && <button disabled={subscriptionStatus.isSubscribed} className='btn' onClick={subscribe}>
                                 {subscriptionStatus.isSubscribed ? 'Subscribed' : 'Subscribe'}
                                 {subscriptionStatus.isSubscribed ? <i className='fa-solid fa-circle-check fa-white'></i> : <i className='fa-solid fa-circle-plus'></i>}
-                            </button>
+                            </button>}
                             {!subscriptionStatus.isSubscribed && <a target='_blank' rel='noreferrer' href={window.location.hostname === 'localhost' ? `http://localhost:7000/api/dataset/data/preview/${datasetId}` : `/api/dataset/data/preview/${datasetId}`} className='btn'>View Preview<i className='fa-solid fa-circle-arrow-right'></i></a>}
                             {subscriptionStatus.isSubscribed && <a target='_blank' rel='noreferrer' href={window.location.hostname === 'localhost' ? `http://localhost:7000/api/dataset/data/view/${datasetId}/${subscriptionStatus.subscriptionId}` : `/api/dataset/data/view/${datasetId}/${subscriptionStatus.subscriptionId}`} className='btn'>View Dataset<i className='fa-solid fa-circle-arrow-right'></i></a>}
                         </div>
