accceecc137f89643fe031fc2e76ba4d4c841937
Arnab Hazra <arnhazra@gmail.com>
2023-02-18 11:24:20 +0530

    autodeploy

--- app.js
+++ app.js
@@ -2,23 +2,18 @@ const express = require('express')
 const Connection = require('./functions/Connection')
 const dotenv = require('dotenv').config()
 const fs = require('fs')
+const path = require('path')
 
 const handler = express()
 handler.listen(process.env.PORT)
-
 handler.use(express.json({ extended: false, limit: '50mb' }))
-
 Connection()
-
 fs.readdirSync('./api').map(route => handler.use(`/api/${route.split('.')[0].toLowerCase()}`, require(`./api/${route.split('.')[0]}`)))
 
+
 if (process.env.NODE_ENV == 'production') {
-    const root = require('path').join(__dirname, 'views', 'build')
-    handler.use(express.static(root))
+    handler.use(express.static(path.join(__dirname, '../views/build')))
     handler.get('*', (req, res) => {
-        res.sendFile('index.html', { root })
-    })
-    handler.get('/static/js/*', (req, res) => {
-        res.sendFile(req.url, { root })
+        res.sendFile(path.join(__dirname + '../views/build/index.html'))
     })
 }
