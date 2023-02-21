cef98d32a57771127d6f9fc45512cdd76e11e3d6
Arnab Hazra <arnhazra@gmail.com>
2023-02-21 22:32:03 +0530

    autodeploy

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ const cors = require('cors')
 const dotenv = require('dotenv').config()
 const fs = require('fs')
 
-const corsOptions = {
-    origin: ['http://lenstack.vercel.app', 'http://localhost:3000']
-}
-
 const app = express()
 app.listen(process.env.PORT)
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json({ extended: false, limit: '5mb' }))
 Connection()
 fs.readdirSync('./api').map(route => app.use(`/api/${route.split('.')[0].toLowerCase()}`, require(`./api/${route.split('.')[0]}`)))
