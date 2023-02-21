9441ee68c8111919fd874d5eed1c25eb06f1a69a
Arnab Hazra <arnhazra@gmail.com>
2023-02-21 22:17:15 +0530

    autodeploy

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const Connection = require('./functions/Connection')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 const fs = require('fs')
 
+const corsOptions = {
+    origin: ['http://lenstack.vercel.app', 'http://localhost:3000']
+}
+
 const app = express()
-const root = require('path').join(__dirname, 'views', 'build')
-app.use(express.static(root));
-app.use(cors())
 app.listen(process.env.PORT)
-app.use(express.json({ extended: false, limit: '50mb' }))
+app.use(cors(corsOptions))
+app.use(express.json({ extended: false, limit: '5mb' }))
 Connection()
 fs.readdirSync('./api').map(route => app.use(`/api/${route.split('.')[0].toLowerCase()}`, require(`./api/${route.split('.')[0]}`)))
-app.get("*", (req, res) => { res.sendFile('index.html', { root }); })
--- views/src/config/index.ts
+++ views/src/config/index.ts
@@ -1,3 +1,3 @@
-const urlPathName = process.env.NODE_ENV === 'production' ? 'https://lenstack-api.vercel.app' : ''
+const urlPathName = process.env.NODE_ENV === 'production' ? 'https://lenstack-api.vercel.app' : 'http://localhost:7000'
 
 export default urlPathName
