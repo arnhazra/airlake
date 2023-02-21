c0721ee0b0e9761ccd84d8cffe65b34c686802f9
Arnab Hazra <arnhazra@gmail.com>
2023-02-21 22:26:34 +0530

    autodeploy

--- views/src/config/index.ts
+++ views/src/config/index.ts
@@ -1,3 +1,3 @@
-const urlPathName = process.env.NODE_ENV === 'production' ? 'https://lenstack-api.vercel.app' : 'http://localhost:7000'
+const urlPathName = process.env.NODE_ENV === 'production' ? 'https://lenstack-api.vercel.app' : 'https://lenstack-api.vercel.app'
 
 export default urlPathName
