5c7547c545b50dad387079e42d9ccd44d0b42a0a
Arnab Hazra <arnhazra@gmail.com>
2023-02-25 01:32:20 +0530

    autodeploy

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ app.use(express.json({ extended: false, limit: '5mb' }))
 Connection()
 fs.readdirSync('./api').map(route => app.use(`/api/${route.split('.')[0].toLowerCase()}`, require(`./api/${route.split('.')[0]}`)))
 
-// if (process.env.NODE_ENV == 'production') {
-//     const root = require('path').join(__dirname, 'views', 'build')
-//     app.use(express.static(root))
-//     app.get('*', (req, res) => {
-//         res.sendFile('index.html', { root })
-//     })
-// }
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'views', 'build')));
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'views', 'build', 'index.html'));
+if (process.env.NODE_ENV == 'production') {
+    const root = require('path').join(__dirname, 'views', 'build')
+    app.use(express.static(root))
+    app.get('*', (req, res) => {
+        res.sendFile('index.html', { root })
     })
 }
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'views', 'build')));
+//     app.get('/*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'views', 'build', 'index.html'));
+//     })
+// }
