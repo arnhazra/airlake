e39acc723fd6b3a0b9e3a96b1cacd700f035477b
Arnab Hazra <arnhazra@gmail.com>
2023-03-01 23:40:08 +0530

    autodeploy

--- views/src/constants/Constants.ts
+++ views/src/constants/Constants.ts
@@ -1,9 +1,11 @@
 enum Constants {
-    HomeHeader1 = 'Accelerate your research,',
+    HomeTop = 'Empowering Innovation Together',
+    HomeHeader1 = 'Build better models,',
     HomeHeader2 = `with the diverse`,
-    HomeHeader3 = `collection of datasets`,
+    HomeHeader3 = `ledger of datasets`,
     HomeIntro1 = `Maximize your data-driven insights with datasets`,
     HomeIntro2 = `Find the perfect dataset for your needs, get started now`,
+    HomeFooter = '#BUILTOnEthereum',
     Info = 'To Add LST in your Metamask wallet, switch to Goerli Test Network and Import Token 0x9d46e816e10c6353Fcf7381343b053117641d472',
     Warning = 'Do not use mainnet ethers to buy LST, it is just an implementation POC for ERC-20 standard.',
     SyncCompleteMessage = `Sync Complete`,
