b1c84e10438f5a6e53813b72a9f0843425c183f9
Arnab Hazra <arnhazra@gmail.com>
2023-03-05 23:27:32 +0530

    autodeploy

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken')
 const statusMessages = require('../constants/Messages')
 const { getTokenFromRedis } = require('../functions/UseRedis')
-const UserModel = require('../models/UserModel')
 const dotenv = require('dotenv').config()
 
 const JWT_SECRET = process.env.JWT_SECRET
@@ -17,38 +16,20 @@ module.exports = async function (req, res, next) {
         try {
             const decoded = jwt.verify(accessToken, JWT_SECRET)
             req.id = decoded.id
-            const user = await UserModel.findById(decoded.id)
+            const redisAccessToken = await getTokenFromRedis(req.id)
 
-            if (user) {
-                const redisAccessToken = await getTokenFromRedis(user.id)
-                if (!redisAccessToken) {
-                    return res.status(401).json({ msg: statusMessages.invalidToken })
-                }
-
-                else if (redisAccessToken === accessToken) {
-                    next()
-                }
-
-                else {
-                    return res.status(401).json({ msg: statusMessages.invalidToken })
-                }
+            if (redisAccessToken === accessToken) {
+                next()
             }
 
             else {
-                return res.status(401).json({ msg: statusMessages.invalidUser })
+                throw new jwt.TokenExpiredError
             }
         }
 
         catch (error) {
-            if (error.name) {
-                if (error.name === 'JsonWebTokenError' || error.name === 'SyntaxError') {
-                    return res.status(401).json({ msg: statusMessages.invalidToken })
-                }
-
-                else {
-
-                    return res.status(500).json({ msg: statusMessages.connectionError })
-                }
+            if (error instanceof jwt.JsonWebTokenError || error instanceof SyntaxError || error instanceof jwt.TokenExpiredError) {
+                return res.status(401).json({ msg: statusMessages.invalidToken })
             }
 
             else {
