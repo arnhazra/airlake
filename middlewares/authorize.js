e08501ee49bcdabd6a730832f2119b4a6db17798
Arnab Hazra <arnhazra@gmail.com>
2023-03-09 01:50:23 +0530

    autodeploy

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const statusMessages = require('../constants/Messages')
 const { getTokenFromRedis } = require('../functions/UseRedis')
 const dotenv = require('dotenv').config()
-const JWT_SECRET = process.env.JWT_SECRET
+const rsaPublicKey = process.env.RSA_PUBLIC_KEY
 
 module.exports = async function (req, res, next) {
     const accessToken = req.headers['authorization']?.split(' ')[1]
@@ -13,7 +13,7 @@ module.exports = async function (req, res, next) {
 
     else {
         try {
-            const decoded = jwt.verify(accessToken, JWT_SECRET)
+            const decoded = jwt.verify(accessToken, rsaPublicKey, { algorithms: ['RS512'] })
             req.id = decoded.id
             const redisAccessToken = await getTokenFromRedis(req.id)
 
@@ -27,6 +27,7 @@ module.exports = async function (req, res, next) {
         }
 
         catch (error) {
+            console.log(error)
             if (error instanceof jwt.JsonWebTokenError || error instanceof SyntaxError || error instanceof jwt.TokenExpiredError) {
                 return res.status(401).json({ msg: statusMessages.invalidToken })
             }
