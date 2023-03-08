6313c5736c781be1f8b2b9b8ef9eca3be0beb908
Arnab Hazra <arnhazra@gmail.com>
2023-03-09 01:32:39 +0530

    revert

--- constants/Origins.js
+++
@@ -1,6 +0,0 @@
-const origins = {
-    developmentOrigin: 'http://localhost:3000',
-    productionOrigin: 'https://lenstack.vercel.app'
-}
-
-module.exports = origins
--- views/src/constants/Endpoints.ts
+++ views/src/constants/Endpoints.ts
@@ -1,24 +1,18 @@
-enum endpointHostName {
-    development = 'http://localhost:7000',
-    production = 'https://lenstack.vercel.app'
-}
-const currentEndpointHostName = process.env.NODE_ENV === 'production' ? endpointHostName.production : endpointHostName.development
-
 const endPoints = {
-    generateAuthCodeEndpoint: `${currentEndpointHostName}/api/auth/generateauthcode`,
-    verifyAuthCodeEndpoint: `${currentEndpointHostName}/api/auth/verifyauthcode`,
-    signOutEndpoint: `${currentEndpointHostName}/api/auth/signout`,
-    checkAuthEndpoint: `${currentEndpointHostName}/api/auth/checkauth`,
-    createTxEndpoint: `${currentEndpointHostName}/api/wallet/createtx`,
-    walletTransactionEndpoint: `${currentEndpointHostName}/api/wallet/transactions`,
-    walletLivePriceEndpoint: `${currentEndpointHostName}/api/wallet/getliveprice`,
-    datasetSortAndFilterOptionsEndpoint: `${currentEndpointHostName}/api/dataset/getsortandfilteroptions`,
-    datasetLibraryEndpoint: `${currentEndpointHostName}/api/dataset/library`,
-    datasetSubscriptionEndpoint: `${currentEndpointHostName}/api/dataset/mysubscriptions`,
-    datasetViewEndpoint: `${currentEndpointHostName}/api/dataset/viewone`,
-    findsimilarDatasets: `${currentEndpointHostName}/api/dataset/findsimilar`,
-    subscribeEndpoint: `${currentEndpointHostName}/api/subscription/subscribe`,
-    checkSubscriptionEndpoint: `${currentEndpointHostName}/api/subscription/issubscribed`,
+    generateAuthCodeEndpoint: `/api/auth/generateauthcode`,
+    verifyAuthCodeEndpoint: `/api/auth/verifyauthcode`,
+    signOutEndpoint: `/api/auth/signout`,
+    checkAuthEndpoint: `/api/auth/checkauth`,
+    createTxEndpoint: `/api/wallet/createtx`,
+    walletTransactionEndpoint: `/api/wallet/transactions`,
+    walletLivePriceEndpoint: `/api/wallet/getliveprice`,
+    datasetSortAndFilterOptionsEndpoint: `/api/dataset/getsortandfilteroptions`,
+    datasetLibraryEndpoint: `/api/dataset/library`,
+    datasetSubscriptionEndpoint: `/api/dataset/mysubscriptions`,
+    datasetViewEndpoint: `/api/dataset/viewone`,
+    findsimilarDatasets: `/api/dataset/findsimilar`,
+    subscribeEndpoint: `/api/subscription/subscribe`,
+    checkSubscriptionEndpoint: `/api/subscription/issubscribed`,
     etherScanEndpoint: `https://goerli.etherscan.io/tx`,
 }
 
