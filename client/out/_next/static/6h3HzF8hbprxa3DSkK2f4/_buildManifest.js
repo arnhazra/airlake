832ddb8024ee6e1ad67f2d62a9abdee9e22444e2
Arnab Hazra <arnhazra@gmail.com>
2023-03-10 23:46:03 +0530

    del

--- client/out/_next/static/6h3HzF8hbprxa3DSkK2f4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/299b4710ca2a9192.css","static/chunks/pages/index-b73aca6a8edbce00.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- client/src/components/ReactIfComponent.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, Fragment } from 'react'
-import { ReactIfProps } from '../types/Props'
-
-const ReactIf: FC<ReactIfProps> = ({ condition, children }) => {
-    return condition === undefined || !condition ? <Fragment></Fragment> : <Fragment>{children}</Fragment>
-}
-
-export default ReactIf
