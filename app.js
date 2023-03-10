81cc4a6a60b7036ea0cb31709a8f5e1e58a236a6
Arnab Hazra <arnhazra@gmail.com>
2023-03-10 17:17:55 +0530

    autodeploy

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ app.use('/api/auth', authRouter.getRouter())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'views', 'build')))
-    app.get('/*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'views', 'build', 'index.html'))
+    })
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'views', 'build', 'index.html'));
     })
 }
