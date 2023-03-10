c604c24144fbd7c18f3089a13c9b0622ff8ad12b
Arnab Hazra <arnhazra@gmail.com>
2023-03-10 17:33:09 +0530

    autodeploy

--- app.js
+++ app.js
@@ -26,11 +26,8 @@ app.use('/api/wallet', walletRouter.getRouter())
 app.use('/api/auth', authRouter.getRouter())
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'views', 'build')))
-    app.get('/', (req, res) => {
+    app.use(express.static(path.join(__dirname, 'views', 'build'), { maxAge: 60000 }))
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, 'views', 'build', 'index.html'))
-    })
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'views', 'build', 'index.html'));
     })
 }
