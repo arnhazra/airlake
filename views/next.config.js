1420a0d2b364b4bbdb71bf5134f932b8cb843e15
Arnab Hazra <arnhazra@gmail.com>
2023-03-23 11:52:04 +0530

    autodeploy

--- views/next.config.js
+++ views/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
 	images: {
 		unoptimized: true
 	},
+	exportPathMap: async function (defaultPathMap) {
+		return {
+			'/': { page: '/' },
+			'/account': { page: '/account' },
+			'/auth': { page: '/auth' },
+			'/buycoin': { page: '/buycoin' },
+			'/datasetlibrary': { page: '/datasetlibrary' },
+			'/mysubscriptions': { page: '/mysubscriptions' },
+			'/sellcoin': { page: '/sellcoin' },
+			'/wallet': { page: '/wallet' },
+		};
+	},
 	output: 'export',
 }
 
