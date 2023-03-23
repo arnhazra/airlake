0699b24935e6123a14201e6bcf73168fcbec7f2f
Arnab Hazra <arnhazra@gmail.com>
2023-03-23 16:11:27 +0530

    Delete Endpoints.js

--- constants/Endpoints.js
+++
@@ -1,6 +0,0 @@
-const endPoints = {
-    liveEthPriceEndPoint: 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=inr%2Cusd%2Ceur',
-    tokenIssuer: 'https://lenstack.vercel.app'
-}
-
-module.exports = endPoints
