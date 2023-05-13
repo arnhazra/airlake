f2454b5a659ddbc46800a0c4f017cf9530c7d1fd
Arnab Hazra <arnhazra@gmail.com>
2023-05-13 18:15:31 +0530

    ProdDeploy

--- api/handler.ts
+++ api/handler.ts
@@ -18,7 +18,7 @@ const accountRouter = new AccountRouter()
 const app = express()
 app.listen(process.env.PORT)
 app.use(cors())
-app.use(express.json({ limit: '5mb' }))
+app.use(express.json({ limit: '3mb' }))
 connectMongo()
 connectRedis()
 
