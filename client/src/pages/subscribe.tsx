5d8c2235cef0c1244c3c3e93011fd3bd6a7b456d
Arnab Hazra <arnhazra@gmail.com>
2023-06-23 20:03:47 +0530

    ProdDeploy

--- client/src/pages/subscribe.tsx
+++ client/src/pages/subscribe.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Row } from 'react-bootstrap'
+import { Button, ButtonGroup, Col, Container, Row } from 'react-bootstrap'
 import { Fragment, useContext, useEffect, useState } from 'react'
 import { NextPage } from 'next'
 import jwtDecode from 'jwt-decode'
@@ -10,10 +10,10 @@ import { useRouter } from 'next/router'
 
 const HomePage: NextPage = () => {
     const [{ userState, subPlanState }] = useContext(AppContext)
+    const [selectedPlan, setSelectedPlan] = useState('Pro')
     const [isSubscribeModalOpened, setSubscribeModalOpened] = useState(false)
     const [isUnsubscribeModalOpened, setUnsubscribeModalOpened] = useState(false)
     const [tokenId, setTokenId] = useState('')
-    const router = useRouter()
 
     useEffect(() => {
         try {
@@ -26,54 +26,49 @@ const HomePage: NextPage = () => {
 
     const hideSubscribeModal = () => {
         setSubscribeModalOpened(false)
-        router.push('/dashboard')
     }
 
     const hideUnsubscribeModal = () => {
         setUnsubscribeModalOpened(false)
-        router.push('/dashboard')
     }
 
     return (
         <Fragment>
-            <Container>
-                <div className='cover'>
-                    <p className='text-center display-5'>Subscribe</p>
-                    <Row className='justify-content-center mt-4'>
-                        <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                            <div className='jumbotron'>
-                                <p className='branding text-center'>Free Plan</p>
-                                <p className='display-6 text-center'><i className='fa-brands fa-ethereum'></i>0</p>
-                                <p className='lead'><i className='fa-solid fa-circle-check'></i>Metadata API</p>
-                                <p className='lead'><i className='fa-solid fa-circle-check'></i>Lifetime Free</p>
-                                <Show when={userState.subscriptionKey.length === 0}>
-                                    <Button disabled className='btn-block'>Current Plan <i className='fa-solid fa-circle-check'></i></Button>
-                                </Show>
-                                <Show when={userState.subscriptionKey.length > 0}>
-                                    <Button className='btn-block' onClick={() => setUnsubscribeModalOpened(true)}>Downgrade</Button>
-                                </Show>
-                            </div>
-                        </Col>
-                        <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-                            <div className='jumbotron'>
-                                <p className='branding text-center'>Airlake Pro</p>
-                                <p className='display-6 text-center'><i className='fa-brands fa-ethereum'></i>{subPlanState.proSubscriptionPrice}/mo</p>
-                                <p className='lead'><i className='fa-solid fa-circle-check'></i>Data API</p>
-                                <p className='lead'><i className='fa-solid fa-circle-check'></i>Premium Features</p>
-                                <Show when={userState.subscriptionKey.length === 0}>
-                                    <Button className='btn-block' onClick={() => setSubscribeModalOpened(true)}>Pay & Subscribe</Button>
-                                </Show>
-                                <Show when={userState.subscriptionKey.length > 0}>
-                                    <Button disabled className='btn-block'>Current Plan <i className='fa-solid fa-circle-check'></i></Button>
-                                </Show>
-                            </div>
-                        </Col>
-                    </Row>
+            <div className="box">
+                <p className='branding'>Subscribe</p>
+                <p className='boxtext'>Choose your Plan</p>
+                <ButtonGroup className='btn-group-card'>
+                    <Button className={selectedPlan === 'Free' ? 'btn-grp-btn-sel' : 'btn-grp-btn'} onClick={(): void => setSelectedPlan('Free')}>FREE</Button>
+                    <Button className={selectedPlan === 'Pro' ? 'btn-grp-btn-sel' : 'btn-grp-btn'} onClick={(): void => setSelectedPlan('Pro')}>PRO</Button>
+                </ButtonGroup>
+                <div>
+                    <Show when={selectedPlan === 'Free'}>
+                        <p className='branding text-center'><i className='fa-brands fa-ethereum'></i>0</p>
+                        <p className='lead'><i className='fa-solid fa-circle-check'></i>Metadata API</p>
+                        <p className='lead'><i className='fa-solid fa-circle-check'></i>Lifetime Free</p>
+                        <Show when={userState.subscriptionKey.length === 0}>
+                            <Button disabled className='btn-block'>Current Plan <i className='fa-solid fa-circle-check'></i></Button>
+                        </Show>
+                        <Show when={userState.subscriptionKey.length > 0}>
+                            <Button className='btn-block' onClick={() => setUnsubscribeModalOpened(true)}>Downgrade</Button>
+                        </Show>
+                    </Show>
+                    <Show when={selectedPlan === 'Pro'}>
+                        <p className='branding text-center'><i className='fa-brands fa-ethereum'></i>{subPlanState.proSubscriptionPrice}/mo</p>
+                        <p className='lead'><i className='fa-solid fa-circle-check'></i>Data API</p>
+                        <p className='lead'><i className='fa-solid fa-circle-check'></i>Premium Features</p>
+                        <Show when={userState.subscriptionKey.length === 0}>
+                            <Button className='btn-block' onClick={() => setSubscribeModalOpened(true)}>Pay & Subscribe</Button>
+                        </Show>
+                        <Show when={userState.subscriptionKey.length > 0}>
+                            <Button disabled className='btn-block'>Current Plan <i className='fa-solid fa-circle-check'></i></Button>
+                        </Show>
+                    </Show>
                 </div>
-                <SubscribeModal price={Number(subPlanState.proSubscriptionPrice) * 10000} isOpened={isSubscribeModalOpened} closeModal={() => { hideSubscribeModal() }} />
-                <UnsubscribeModal tokenId={tokenId} refundAmount={Number(subPlanState.proSubscriptionPrice) * 5000} isOpened={isUnsubscribeModalOpened} closeModal={() => { hideUnsubscribeModal() }} />
-            </Container>
-        </Fragment >
+            </div>
+            <SubscribeModal price={Number(subPlanState.proSubscriptionPrice) * 10000} isOpened={isSubscribeModalOpened} closeModal={() => { hideSubscribeModal() }} />
+            <UnsubscribeModal tokenId={tokenId} refundAmount={Number(subPlanState.proSubscriptionPrice) * 5000} isOpened={isUnsubscribeModalOpened} closeModal={() => { hideUnsubscribeModal() }} />
+        </Fragment>
     )
 }
 
